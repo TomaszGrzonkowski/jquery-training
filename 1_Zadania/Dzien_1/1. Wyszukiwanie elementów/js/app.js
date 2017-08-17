47448e1f40ca02782be807f46c4f1c770fa09a0a
Jakub Barczyk <jakubbarczyk@yahoo.com>
2017-08-17 18:36:07 +0200

    d1-s2

--- 1_Zadania/Dzien_1/1. Wyszukiwanie elementów/js/app.js
+++ 1_Zadania/Dzien_1/1. Wyszukiwanie elementów/js/app.js
@@ -2,8 +2,13 @@ $(function () {
     console.info('DOM zaladowany!');
 
     setBackgroundElementOnSection();
+    setHoverOnNavInLinksSection();
 });
 
 function setBackgroundElementOnSection() {
     $('section').addClass('backgroundElement');
+}
+
+function setHoverOnNavInLinksSection() {
+    $('.links').find('nav').addClass('hover-effect');
 }
