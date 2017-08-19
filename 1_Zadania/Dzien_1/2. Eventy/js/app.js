adcb75a6a0fe6c972582d037624b330424333954
Jakub Barczyk <jakubbarczyk@yahoo.com>
2017-08-19 10:31:12 +0200

    d1-e2

--- 1_Zadania/Dzien_1/2. Eventy/js/app.js
+++ 1_Zadania/Dzien_1/2. Eventy/js/app.js
@@ -1,7 +1,12 @@
 $(function () {
     $('dt').on('mouseenter', showElementInfo);
+    $('.hero-buttons').find('button').on('click', showElementData);
 });
 
 function showElementInfo(event) {
     console.info(event.target.nodeName, event.target.innerHTML);
+}
+
+function showElementData(event) {
+    console.log($(event.target).data('feature'));
 }
